e01a75b5162c358f97826984a374e21a5d80e0c7
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2019-05-24 19:41:59 -0400

    Added tutorial skeleton

---
+++ tutorial/api/book_store.py
@@ -0,0 +1,23 @@
+content = [{
+    'category': 'reference',
+    'price': 8.95,
+    'title': 'Sayings of the Century',
+    'author': 'Nigel Rees'
+}, {
+    'category': 'fiction',
+    'price': 12.99,
+    'title': 'Sword of Honour',
+    'author': 'Evelyn Waugh'
+}, {
+    'category': 'fiction',
+    'price': 8.99,
+    'title': 'Moby Dick',
+    'isbn': '0-553-21311-3',
+    'author': 'Herman Melville'
+}, {
+    'category': 'fiction',
+    'price': 22.99,
+    'title': 'The Lord of the Rings',
+    'isbn': '0-395-19395-8',
+    'author': 'J. R. R. Tolkien'
+}]
---
+++ tutorial/readme.md
@@ -0,0 +1,5 @@
+#
+
+## Slides
+
+To view the slides just open them on the browser.
